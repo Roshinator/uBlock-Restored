38f346fefa1b3295e7a70539bc475bfa239e6a70
rosh324 <sevaliaroshan@gmail.com>
2019-12-07 23:07:51 -0800

    Added content blocker target

---
+++ uBlock Restored Content Blocker/ContentBlockerRequestHandler.h
@@ -0,0 +1,13 @@
+//
+//  ContentBlockerRequestHandler.h
+//  uBlock Restored Content Blocker
+//
+//  Created by Roshan Sevalia on 12/7/19.
+//  Copyright © 2019 Roshan Sevalia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ContentBlockerRequestHandler : NSObject <NSExtensionRequestHandling>
+
+@end
---
+++ uBlock Restored Content Blocker/ContentBlockerRequestHandler.m
@@ -0,0 +1,26 @@
+//
+//  ContentBlockerRequestHandler.m
+//  uBlock Restored Content Blocker
+//
+//  Created by Roshan Sevalia on 12/7/19.
+//  Copyright © 2019 Roshan Sevalia. All rights reserved.
+//
+
+#import "ContentBlockerRequestHandler.h"
+
+@interface ContentBlockerRequestHandler ()
+
+@end
+
+@implementation ContentBlockerRequestHandler
+
+- (void)beginRequestWithExtensionContext:(NSExtensionContext *)context {
+    NSItemProvider *attachment = [[NSItemProvider alloc] initWithContentsOfURL:[[NSBundle mainBundle] URLForResource:@"blockerList" withExtension:@"json"]];
+    
+    NSExtensionItem *item = [[NSExtensionItem alloc] init];
+    item.attachments = @[attachment];
+    
+    [context completeRequestReturningItems:@[item] completionHandler:nil];
+}
+
+@end
